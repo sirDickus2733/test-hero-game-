1da651641e6a4af18e7f3af3f75716f660990797
Ndamulelo Nemakhavhani <ndamulelo@bbd.co.za>
2021-10-01 13:40:55 +0200

    add user settiongs

---
+++ HeroVsGoblin01/HeroVsGoblin01/Common/EmptyTile.cs
@@ -0,0 +1,13 @@
+﻿namespace HeroVsGoblin01.Common
+{
+  /// <summary>
+  /// Represent a mere empty tile on the game map
+  /// </summary>
+  public class EmptyTile: Tile
+  {
+    public EmptyTile(int xValue, int yValue) : base(xValue, yValue)
+    {
+
+    }
+  }
+}
---
+++ HeroVsGoblin01/HeroVsGoblin01/Common/Obstacle.cs
@@ -0,0 +1,16 @@
+﻿using HeroVsGoblin01.Properties;
+
+namespace HeroVsGoblin01.Common
+{
+  /// <summary>
+  /// Borders the game map with obstacles that no characters can move past
+  /// </summary>
+  public class Obstacle:Tile
+  {
+    #region Constructor(s)
+    public Obstacle(int xValue, int yValue):base(xValue, yValue, Settings.Default.ObstacleSymbol)
+    {
+    }
+    #endregion
+  }
+}
